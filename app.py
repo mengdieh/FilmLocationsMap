53b69988aec5451ad37d8e7d8802d9a7a55186eb
mengdieh <mengdie.hu@gmail.com>
2014-12-03 22:12:20 -0500

    finished

--- app.py
+++ app.py
@@ -4,35 +4,52 @@ import csv, json
 
 app = Flask(__name__)
 
-# load movie data
+# loads movie data from the csv file
 movie_data = {}
+all_locations = []
 with open('sfmovies.csv') as csvfile:
 	rdr = csv.DictReader(csvfile)
+	i = 0
 	for row in rdr:
 		title_norm = row['Title'].lower()
+		all_locations.append({ \
+				'LocationId': i, \
+				'Title': row['Title'], \
+				'Release Year': row['Release Year'], \
+				'Director': row['Director'], \
+				'Writer': row['Writer'], \
+				'LocationText':row['Locations'], \
+				'Latitude':row['Latitude'], \
+				'Longitude':row['Longitude'], \
+				'Fun Facts':row['Fun Facts']
+			})
 		if title_norm not in movie_data:
-			movie_data[title_norm] = {
-				'Title': row['Title'],
-				'Release Year': row['Release Year'],
-				'Director': row['Director'],
-				'Writer': row['Writer'],
-				'Locations':[{'Location':row['Locations'], 'Fun Facts':row['Fun Facts']}]
+			movie_data[title_norm] = { \
+				'Title': row['Title'], \
+				'Release Year': row['Release Year'], \
+				'Director': row['Director'], \
+				'Writer': row['Writer'], \
+				'LocationIds':[i] \
 			}
 		else:
-			movie_data[title_norm]['Locations'].append({'Location':row['Locations'], 'Fun Facts':row['Fun Facts']})
+			movie_data[title_norm]['LocationIds'].append(i)
+		i = i + 1
 
 
+# index.html is the main template for this app
 @app.route('/')
 @app.route('/index')
 def index():
 	return render_template('index.html')
 
-# returns info for all movies from the db
+
+# api routes:
+# returns all movie titles in an array
 @app.route('/api/titles')
-def all():
+def titles():
 	return jsonify({'titles':[movie['Title'] for movie in movie_data.values()]})
 
-# returns info for given movie
+# returns info for a given movie
 @app.route('/api/movie/<title>')
 def movie(title=None):
 	movie = {}
@@ -42,5 +59,27 @@ def movie(title=None):
 		pass
 	return jsonify(movie)
 
+# returns info for all movies in the database
+@app.route('/api/movies')
+def allMovies():
+	return jsonify(movie_data)
+
+# returns all locations in the database
+@app.route('/api/locations')
+def getAllLocations():
+	return jsonify({'locations':all_locations})
+
+# returns all locations for a given movie
+@app.route('/api/locations/<title>')
+def getLocations(title=None):
+	locations = []
+	try:
+		movie = movie_data[title]
+		locations = [ all_locations[i] for i in movie['LocationIds'] ]
+	except KeyError:
+		pass
+	return jsonify({'locations':locations})
+
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=False)
--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,172 @@
 $(function() {
-/*------ using typeahead.js and bloodhound.js for autocompletion ---------*/
+/*----------- uses backbone.js to add/clean markers on map -------------*/
+
+// the collection of locations - the model collection of the MVC framework
+var LocationList = Backbone.Collection.extend({
+  url: '/api/locations',
+
+  initialize: function() {
+    this.allLocations = [];
+  },
+
+  fetch: function() {
+    // loads all movie locations
+    var self = this;
+    $.getJSON('/api/locations', function(response) {
+      self.allLocations = response['locations'];
+      self.reset(self.allLocations);
+    });
+  }
+});
+
+var locations = new LocationList();
+
+// a MarkerView is tied to each location
+var MarkerView = Backbone.View.extend({
+  initialize: function(options) {
+    this.map = options.map;
+    MarkerView.infowindow = null;
+
+    if( !this.model.Latitude || !this.model.Longitude ) { 
+      // if lat&lng are not preloaded, load them first
+      // use setTimeOut to avoid OVER_QUERY_LIMIT
+      setTimeout(function() {
+        geocoder.geocode({'address': this.model.LocationText + ", San Francisco, CA"}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            var loc = results[0].geometry.location;
+            this.model.Latitude = loc.lat();
+            this.model.Longitude = loc.lng();
+            createMarker();
+          } 
+        });
+      }, 2500);
+    }
+    else
+      this.createMarker();
+  },
+
+  createMarker: function() {
+    // creates a marker
+    var latlng = new google.maps.LatLng(this.model.Latitude, this.model.Longitude);
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: this.map,
+      title: this.model.Title,
+    // animation: google.maps.Animation.DROP
+    });
+    this.marker = marker;
+
+    // creates an infowindow
+    var contentString = '<h5>' + this.model.Title + ' (' + this.model['Release Year']+ ')</h5>'
+                      + '<p>Director: ' + this.model.Director + '</br>'
+                      + 'Location: ' + this.model.LocationText;
+    if( this.model['Fun Facts'] ) 
+      contentString += '</br>' + 'Fun Fact: ' + this.model['Fun Facts'] + '</p>';
+    else
+      contentString += '</p>';                  
+
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+    });
+
+    // shows infowindow when clicking on the marker
+    // MarkerView.infowindow records the last open infowindow
+    // always closes the last infowindow before opening a new one
+    google.maps.event.addListener(marker, 'click', function() {
+      if(MarkerView.infowindow)
+        MarkerView.infowindow.close();
+      MarkerView.infowindow = infowindow;
+      infowindow.open(this.map, marker);
+    });    
+  },
+
+  show: function() {
+    this.marker.setMap(this.map);
+  },
+
+  hide: function() {
+    this.marker.setMap(null);
+  },
+})
+
+// top-level view of the map
+var AppView = Backbone.View.extend({
+  initialize: function() {
+    this.loadMap();
+    this.collection = locations; 
+    this.markers = [];
+    this.listenTo(this.collection, 'reset', this.render )
+    this.collection.fetch(); // loads the entire movie collection on page load to polulate the map
+  },
+
+  loadMap: function() {
+  // loads google map
+    var mapOptions = {
+      center: new google.maps.LatLng(37.7577,-122.4376), // San Francisco
+      scrollWheel: false,
+      zoom: 12
+    };
+    this.map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  },
+
+  render: function() {
+  // renders markers for all locations in the collection
+    // in the first round, create markers for all known locations
+    if( this.markers.length == 0 && this.collection.allLocations.length > 0 ) {
+      for(var i = 0; i < this.collection.allLocations.length; i++ ) {
+        this.markers.push(new MarkerView({model:this.collection.allLocations[i], map:this.map}));
+      }      
+    }
+
+    // clears map
+    for(var i = 0; i < this.markers.length; i++ )
+      this.markers[i].hide();
+
+    // turns on markers for the current collection and centers map around the visible markers
+    var bounds = new google.maps.LatLngBounds();
+    for(var i = 0; i < this.collection.models.length; i++ ) {
+      var markerView = this.markers[this.collection.at(i).attributes.LocationId];
+      markerView.show();
+      bounds.extend(markerView.marker.position);
+    }
+    this.map.fitBounds(bounds);
+  },
+
+  // showLocations() and showAllLocations() make changes to the model,
+  // the map view adjusts itself when the model fires the 'reset' event
+  showLocations: function(locationIds) {
+    locations.reset(locationIds);
+  },
+
+  showAllLocations: function() {
+    locations.reset(locations.allLocations);
+  },
+})
+
+var appView = new AppView();
+
+// the router addes #title to the url when a movie is selected
+var Router = Backbone.Router.extend({
+  routes: {
+    ":query": "selectMovie",
+    "": "selectAll"
+  },
+  selectAll: function() {
+    appView.showAllLocations();
+  },
+  selectMovie: function(query) {
+    $.getJSON('/api/locations/' + query.toLowerCase(), function(response) {
+      appView.showLocations(response.locations);
+    });
+  }
+});
+
+var router = new Router();
+
+Backbone.history.start();
+
+
+/*------ using typeahead.js and bloodhound.js for autocompletion ------*/
 // constructs the suggestion engine
 var engine = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
@@ -12,7 +179,6 @@ var engine = new Bloodhound({
     }
   }
 });
-// kicks off the loading/processing of `local` and `prefetch`
 engine.initialize();
 
 $('#bloodhound .typeahead').typeahead({
@@ -26,100 +192,8 @@ $('#bloodhound .typeahead').typeahead({
   // `ttAdapter` wraps the suggestion engine in an adapter that
   // is compatible with the typeahead jQuery plugin
   source: engine.ttAdapter()
-});
-
-/*-------------------- end autocompletion ------------------------------*/
-
-/*---------- using backbone.js to add/clean markers on map -------------*/
-
-// the collection of locations, each movie can have multiple locations
-var LocationList = Backbone.Collection.extend({
-
-});
-
-var locations = new LocationList();
-
-// top-level view of the map
-var AppView = Backbone.View.extend({
-  initialize: function() {
-
-  },
-
-  render: function() {
-
-  }
-
-
 })
-
-var appView = new AppView();
-
-// router
-var Router = Backbone.Router.extend({
-  routes: {
-    "movie:query": "markMovie",
-    "": "markAll"
-  },
-  initialize: function() {
-    //appView.render();
-  },
-  markAll: function() {
-    
-  },
-  markMovie: function() {
-
-  }
-});
-
-var router = new Router();
-
-//Backbone.history.start();
-        
-/* google maps -----------------------------------------------------*/
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-function initialize() {
-
-  var latlng1 = new google.maps.LatLng(52.3731, 4.8922);
-  var latlng2 = new google.maps.LatLng(59.32522, 18.07002);
-
-  var mapOptions = {
-    center: latlng1,
-    scrollWheel: false,
-    zoom: 13
-  };
-  
-  var bounds = new google.maps.LatLngBounds();
-
-  var marker = new google.maps.Marker({
-    position: latlng1,
-    title: "Amsterdam",
-//    animation: google.maps.Animation.DROP
+.on('typeahead:selected', function(e, data){ // when selected, navigate to /#title
+    router.navigate(data.value, {trigger:true});
   });
-
-  var infowindow = new google.maps.InfoWindow({
-      content: '<p>yadayada</p>'
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.open(map,marker);
-  });
-
-  bounds.extend(marker.position);
-
-  var marker2 = new google.maps.Marker({
-    position: latlng2,
-    title: "Stockholm",
-  });
-  bounds.extend(marker2.position);
-  
-  var map = new google.maps.Map(document.getElementById("map-canvas"));//, mapOptions);
-  marker.setMap(map);
-  marker2.setMap(map);
-  map.fitBounds(bounds);
-};
-
-/* end google maps -----------------------------------------------------*/
-        
 });
